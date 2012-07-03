27321af48f8be9e3c8470f3769753fbc47a25e8a
Alexandre Roman <alexandre.roman@gmail.com>
2012-07-03 16:43:35 +0200

    Fix I18N string for folder deletion

--- src/com/ngarside/memo/ListActivity.java
+++ src/com/ngarside/memo/ListActivity.java
@@ -277,7 +277,7 @@ public class ListActivity extends Activity {
             	} else {
                 	new AlertDialog.Builder(this)
                     .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle(getString(R.string.delete) + fileFolders[spinnerSelection - 2].getName() + "?")
+                    .setTitle(String.format(getString(R.string.delete_title), fileFolders[spinnerSelection - 2].getName()))
                     .setMessage(getString(R.string.delete_confirm))
                     .setPositiveButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
